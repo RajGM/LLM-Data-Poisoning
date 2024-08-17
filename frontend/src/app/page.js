59da87e4ac33cf441f96cbe835a8857bac0a92d9
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2024-08-17 15:23:32 +0400

    Fix: fixed the env

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${process.env.BACKEND_URL}`, {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
